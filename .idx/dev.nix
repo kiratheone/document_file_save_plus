0bea6280a2685201329276a143531c26dfdf6a6d
arikarik.ae <arikarik.ae@gmail.com>
2024-09-02 13:09:35 +0000

    add idx

---
+++ .idx/dev.nix
@@ -0,0 +1,27 @@
+{pkgs}: {
+  channel = "stable-23.11";
+  packages = [
+    pkgs.nodePackages.firebase-tools
+    pkgs.jdk17
+    pkgs.unzip
+  ];
+  idx.extensions = [
+    "Dart-Code.dart-code"
+    "Dart-Code.flutter"];
+  idx.previews = {
+    previews = {
+      android = {
+        command = [
+          "flutter"
+          "run"
+          "--machine"
+          "-d"
+          "android"
+          "-d"
+          "emulator-5554"
+        ];
+        manager = "flutter";
+      };
+    };
+  };
+}
